1ee59e1a36aba2a40d78d2826f0eb2fe344017fe
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-08-25 02:09:11 +0300

    add parsers and connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
+import { config } from "dotenv";
+config({ path: "../.env" });
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+const app = express();
 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+const url = process.env.DB_URL;
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(port, () =>
+      console.log(`Server up and running on port: ${port}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
