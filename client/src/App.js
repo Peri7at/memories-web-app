5ce9bd133772fc342ed24e9c28d77ee6785a0bda
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-08-28 23:18:53 +0300

    create add post functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import { useDispatch } from "react-redux";
+
+import { getPosts } from "./actions/posts";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import memories from "./images/memories.png";
@@ -7,6 +10,11 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Container>
       <AppBar className={classes.appBar} position="static" color="inherit">
