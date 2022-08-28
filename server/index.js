72c5e2af4ac340c6a1136a58c56b366002926827
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-08-28 23:19:54 +0300

    fix bug

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const port = process.env.PORT || 3000;
 const url = process.env.DB_URL;
