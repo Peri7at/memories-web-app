2a22b750a723d1307c1656ef6ae872e89ad36a82
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-09-13 23:58:51 +0300

    modify posts/post component

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,35 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
+
 import useStyles from "./styles";
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <h1>Post</h1>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+    </Card>
+  );
 };
 
 export default Post;
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 
@@ -8,12 +9,21 @@ const Posts = () => {
   const classes = useStyles();
 
   console.log(posts);
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
