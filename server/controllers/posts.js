206f6c7ef5fad196bf40cd2c8847b443fe016dd0
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-09-23 23:46:19 +0300

    add deletePost controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -26,8 +26,22 @@ export const updatePost = async (req, res) => {
   const post = req.body;
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send("No post with that ID");
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
-    new: true,
-  });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
   res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that ID");
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted successfully!" });
 };
