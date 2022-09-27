73ba030f7c275d703ddf4ee385808e75ed372732
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-09-27 22:42:44 +0300

    add like post controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,3 +45,16 @@ export const deletePost = async (req, res) => {
 
   res.json({ message: "Post deleted successfully!" });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that ID");
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+  res.json(updatedPost);
+};
